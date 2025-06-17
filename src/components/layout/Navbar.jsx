bc9d71d6d92a03f2149a939d00f6580aba56de72
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-17 13:15:58 +0530

    Navbar 50% is complete

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import { ChevronUp, Search } from 'lucide-react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className="font-[Montserrat] font-normal max-w-screen-xl mx-auto flex flex-col gap-2 my-3">
+        <div className="flex justify-end items-center gap-12">
+          <p className="cursor-pointer active:scale-95">For Companies</p>
+          <p className="cursor-pointer active:scale-95">Events</p>
+          <p className="cursor-pointer active:scale-95">Become a Master</p>
+          <p className="cursor-pointer active:scale-95">Blogs</p>
+        </div>
+      </div>
+      <hr className="border-[#E5E5E5] border-b-[1px]" />
+      <div className="font-[Montserrat] font-normal max-w-screen-xl mx-auto flex flex-col gap-2 my-3">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-2 h-10 w-10 font-[LeagueSpartan] font-extrabold">
+            <img src="/Images/logo.png" alt="logo" />
+            TSC
+          </div>
+          <div className="flex justify-end items-center gap-12">
+            <p className="flex items-center cursor-pointer active:scale-95">
+              What we do <ChevronUp />
+            </p>
+            <p className="cursor-pointer active:scale-95">About Us</p>
+            <p className="cursor-pointer active:scale-95">Careers</p>
+            <p className="cursor-pointer active:scale-95">Contact Us</p>
+            <p className="flex items-center gap-2 cursor-pointer active:scale-95 px-4 py-2 rounded-lg bg-gradient-to-r from-[#A468DA] to-[#149BF5] text-white">
+              <Search size={16} />
+              Search
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 function Home() {
-    return (
-        <div>
-            <h1 className="font-[Poppins] font-normal text-2xl">This is Poppins font (h1)</h1>
-            <h2 className="font-[Montserrat] font-normal text-xl">This is Montserrat font (h2)</h2>
-            <p className="font-[inter] font-medium">This is Inter font (p)</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="font-[Poppins] font-normal text-2xl">This is Poppins font (h1)</h1>
+      <h2 className="font-[Montserrat] font-normal text-xl">This is Montserrat font (h2)</h2>
+      <p className="font-[inter] font-medium">This is Inter font (p)</p>
+    </div>
+  );
 }
-
 
 export default Home;
