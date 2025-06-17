bbe23963862679e851feb5ae607dd9f40122b9fc
mayur20022 <mtalewar2002@gmail.com>
2025-06-17 11:51:29 +0530

    Added 3 fonts (Inter, Poppins, Montserrat)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,14 @@
-const Home = () => <div className="text-3xl">Home Page</div>;
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1 className="font-[Poppins] font-normal text-2xl">This is Poppins font (h1)</h1>
+            <h2 className="font-[Montserrat] font-normal text-xl">This is Montserrat font (h2)</h2>
+            <p className="font-[inter] font-medium">This is Inter font (p)</p>
+        </div>
+    );
+}
+
 
 export default Home;
