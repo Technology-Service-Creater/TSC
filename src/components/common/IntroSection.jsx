afdd4b644f2584ebfc52c747d0c9e1ccd6762492
Pranay Meharkule <pranaymeharkule272005@gmail.com.>
2025-06-21 13:29:09 +0530

    done

--- src/components/common/IntroSection.jsx
+++ src/components/common/IntroSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function IntoSection({ IntoSectionImage, IntoSectionTitle, IntoSectionDescription }) {
+function IntroSection({ IntroSectionImage, IntroSectionTitle, IntroSectionDescription }) {
   return (
     <section className="w-full relative my-10 max-w-[1194px] h-auto md:h-[408px] mx-auto flex flex-col md:flex-row  gap-4 md:gap-0 overflow-hidden rounded-lg">
       <div
@@ -12,15 +12,15 @@ function IntoSection({ IntoSectionImage, IntoSectionTitle, IntoSectionDescriptio
       />
       <div className="flex-1 flex flex-col  justify-center p-4 md:p-8 gap-4 md:gap-6">
         <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold font-['Poppins']  text-[#181A1B]">
-          {IntoSectionTitle}
+          {IntroSectionTitle}
         </h2>
         <p className="text-sm sm:text-base md:text-[20px] font-['Montserrat'] text-[#181A1B]">
-          {IntoSectionDescription}
+          {IntroSectionDescription}
         </p>
       </div>
       <div className="flex-1 min-h-[180px] md:min-h-0 flex  order-1 md:order-none">
         <img
-          src={IntoSectionImage}
+          src={IntroSectionImage}
           alt="Why TSC"
           className="w-full object-cover  min-h-[180px] max-h-[220px] md:min-h-0 md:max-h-none "
         />
@@ -29,4 +29,4 @@ function IntoSection({ IntoSectionImage, IntoSectionTitle, IntoSectionDescriptio
   );
 }
 
-export default IntoSection;
+export default IntroSection;
