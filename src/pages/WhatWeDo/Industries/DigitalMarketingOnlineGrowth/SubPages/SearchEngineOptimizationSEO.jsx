c48468ab9a3378f4db3b62014fd7dcce531e0133
mayur20022 <mtalewar2002@gmail.com>
2025-06-21 14:56:58 +0530

    Some Changes on common components

--- src/pages/WhatWeDo/Industries/DigitalMarketingOnlineGrowth/SubPages/SearchEngineOptimizationSEO.jsx
+++ src/pages/WhatWeDo/Industries/DigitalMarketingOnlineGrowth/SubPages/SearchEngineOptimizationSEO.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageHeroSection from '../../../../../components/common/PageHeroSection';
 import IntroSection from '../../../../../components/common/IntroSection';
+import { WiTime3 } from 'react-icons/wi';
 
 const contentHeading = 'Search Engine Optimization (SEO)';
 const contentDescription =
@@ -21,6 +22,22 @@ const SearchEngineOptimizationSEO = () => (
         'Boost your website`s visibility and search rankings with a top-tier SEO company. Expert strategies optimize your site for Google, driving organic traffic, enhancing user engagement, and fueling business growth. Whether you need comprehensive SEO solutions or targeted services, partnering with professionals ensures a powerful online presence.g you achieve success effortlessly.'
       }
     />
+    <div className="w-full h-96 px-6 py-4 bg-sky-100 inline-flex justify-center items-center gap-12">
+      <div className="w-96 h-80 px-6 py-4 flex flex-col justify-center items-center gap-6">
+        <div className="w-32 h-32 px-3 py-[5px] rounded-[62.50px] outline-2 outline-offset-[-2px] outline-neutral-800 inline-flex justify-center items-center gap-2">
+          <WiTime3 />
+        </div>
+        <div className="w-full text-center flex flex-col justify-start items-start gap-6">
+          <div className="w-full justify-start text-neutral-800 text-3xl font-bold font-['Poppins'] leading-10">
+            Time-Saving
+          </div>
+          <div className="w-full justify-start text-neutral-800 text-base font-medium font-['Montserrat'] leading-normal tracking-wide">
+            Focus on your business while experts handle your SEO, ensuring optimal results without
+            the hassle.
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
