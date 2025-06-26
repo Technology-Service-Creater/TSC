83ba9b68face59d8ce82e1490ad12db799ce98e3
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-26 16:00:13 +0530

    Add new subpages for Email & Performance Marketing analytics features

---
+++ src/pages/WhatWeDo/Industries/EmailPerformanceMarketing/SubPages/WebMobileAnalytics.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WebMobileAnalytics() {
+  return (
+    <div>
+      <h2>Web & Mobile Analytics</h2>
+      <p>This is the Web & Mobile Analytics subpage under Email & Performance Marketing.</p>
+    </div>
+  );
+}
+
+export default WebMobileAnalytics;
